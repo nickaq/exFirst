f36964364297926cd9aaaf9fa48d206a67bb5890
nickaq <feswork7@gmail.com>
2024-02-23 13:01:15 +0100

    some new updates :D

--- exFirst/ViewController.swift
+++ exFirst/ViewController.swift
@@ -8,9 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var gallery = [#imageLiteral(resourceName: "duy.jpeg"),#imageLiteral(resourceName: "pervaya.jpeg"),#imageLiteral(resourceName: "tretya.jpeg"),#imageLiteral(resourceName: "tree.jpeg")]
+    @IBOutlet weak var showGallery: UIImageView!
+    var index = 0
+    @IBAction func nextButton(_ sender: UIButton) {
+        index += 1
+        if index >= gallery.count {
+            index = 0
+        }
+        updateImage()
+    }
+    
+    @IBAction func prevButton(_ sender: UIButton) {
+        index -= 1
+        if index < 0 {
+            index = gallery.count - 1
+        }
+        updateImage()
+    }
+    func updateImage(){
+        let imageName = gallery[index]
+        showGallery.image = imageName
+    }
+    
+    @IBAction func saveEditButton(_ sender: UIButton) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateImage()
         // Do any additional setup after loading the view.
     }
 
